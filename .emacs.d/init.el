458cc11677fa6b18bbb6c46ef1fb6ad0880592d6
akisute3 <akisute3@gmail.com>
2014-07-06 20:28:48 +0900

    dired-0.el に記述しているコードが init.el に自動挿入されていたので除去

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,3 @@
 ;;; (install-elisp "http://www.rubyist.net/~rubikitch/archive/init-loader-x.el")
 (require 'init-loader-x "~/.emacs.d/elisp/auto-install/init-loader-x")
 (init-loader-load "~/.emacs.d/inits")
-(put 'dired-find-alternate-file 'disabled nil)
