b0ac1399ff56fb8d57d726a58474df38106f9b0d
akisute3 <akisute3@gmail.com>
2014-07-23 22:42:11 +0900

    zencoding-mode から emmet-mode に変更

---
+++ .emacs.d/inits/76_emmet.el
@@ -0,0 +1,12 @@
+;;;; ZenCoding を CSS でも行う emmet-mode
+
+
+;;; (package-install 'emmet-mode)
+(require 'emmet-mode)
+
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+
+(define-key emmet-mode-keymap (kbd "C-m") 'emmet-expand-line)
+(define-key emmet-mode-keymap (kbd "C-j") 'newline-and-indent)
--- .emacs.d/inits/76_zencoding.el
+++
@@ -1,6 +0,0 @@
-;;;; HTML を簡単に記述する ZenCoding
-
-
-;;; (package-install 'zencoding-mode)
-(require 'zencoding-mode)
-(add-hook 'web-mode-hook 'zencoding-mode)
