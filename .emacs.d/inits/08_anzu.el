f32a97705d416521cbec4f8636717ab498e05d0d
akisute3 <akisute3@gmail.com>
2014-07-28 00:00:08 +0900

    検索時にマッチする語数と現在のマッチ位置を表示する anzu 導入

---
+++ .emacs.d/inits/08_anzu.el
@@ -0,0 +1,13 @@
+;;;; 検索コマンド実行中にマッチする語数と現在のマッチ位置表示
+
+
+;;; (package-install 'anzu)
+(require 'anzu)
+(global-anzu-mode +1)
+
+;;; migemo を利用
+(setq anzu-use-migemo t)
+;;; これ以上マッチする語数を数え上げない
+(setq anzu-search-threshold 300)
+;;; この文字以上入力したときに数え上げを行う
+(setq anzu-minimum-input-length 3)
