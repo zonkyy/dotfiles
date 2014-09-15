d5b26f733b2fa9c87af87e9d0c0f49775e32b3f8
akisute3 <akisute3@gmail.com>
2014-09-15 14:00:47 +0900

    magit の ediff で左側に新しいファイルを置く設定が不要なため削除

--- .emacs.d/inits/70_magit.el
+++ .emacs.d/inits/70_magit.el
@@ -7,10 +7,3 @@
 (global-set-key (kbd "C-x v L") 'magit-key-mode-popup-logging)
 (global-set-key (kbd "C-x v z") 'magit-stash)
 (define-key magit-mode-map "\M-l" "l-all")
-
-
-;;; magit の diff で左側に新しいファイルを
-(defadvice magit-ediff* (before swap-a-b activate)
-  (let ((tmp-a (ad-get-arg 0)))
-    (ad-set-arg 0 (ad-get-arg 1))
-    (ad-set-arg 1 tmp-a)))
