6e25d42504a998d03f45e59fb1c484d494838508
akisute3 <akisute3@gmail.com>
2014-09-25 22:48:22 +0900

    helm-ag-r から helm-ag に移行

---
+++ .emacs.d/inits/50_helm-ag.el
@@ -0,0 +1,8 @@
+;;; helm インターフェイスで ag を利用する
+
+
+;;; (package-install 'helm-ag)
+(require 'helm-ag)
+(setq helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
+
+(global-set-key (kbd "C-l a") 'helm-do-ag)
