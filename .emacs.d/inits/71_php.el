f7d348a6f64f2f093bcef1f25662c2d83a7109c6
akisute3 <akisute3@gmail.com>
2014-09-30 00:19:39 +0900

    web-mode の設定が複数のファイルで重複している問題を修正

--- .emacs.d/inits/71_php.el
+++ .emacs.d/inits/71_php.el
@@ -2,8 +2,6 @@
 
 (use-package php-mode
   :ensure php-mode)
-(use-package web-mode
-  :ensure web-mode)
 
 
 (defun php-mode-hooks ()
@@ -13,11 +11,3 @@
   (add-hook 'before-save-hook 'delete-trailing-whitespace))
 
 (add-hook 'php-mode-hook 'php-mode-hooks)
-
-
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
--- .emacs.d/inits/76_web.el
+++ .emacs.d/inits/76_web.el
@@ -2,11 +2,11 @@
 
 (use-package web-mode
   :ensure web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
