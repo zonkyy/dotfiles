fa6a549af94d5ee5ee7ec7f007b254820c4c8ee2
akisute3 <akisute3@gmail.com>
2014-09-30 00:38:55 +0900

    不要な文字コード設定を除去

--- .emacs.d/inits/lin-conf.el
+++ .emacs.d/inits/lin-conf.el
@@ -2,14 +2,7 @@
 
 
 ;;; 言語・文字コード関連の設定
-(set-language-environment "Japanese")
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(setq file-name-coding-system 'utf-8)
 
 
 ;;; フォント設定
