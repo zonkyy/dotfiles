9d7f795bdba338024390a23d5001815586941a75
akisute3 <akisute3@gmail.com>
2014-09-30 00:32:23 +0900

    org-capture の設定を大幅に修正

--- .emacs.d/inits/80_org-capture.el
+++ .emacs.d/inits/80_org-capture.el
@@ -1,43 +1,30 @@
 ;;; org-mode でメモを取る org-capture の設定
 
-
 (use-package org-capture)
+
+(use-package skk-vars)
+(defun today-string ()
+  (skk-current-date
+   (lambda (date-information format gengo and-time)
+     (skk-default-current-date date-information nil 0 'gengo 0 0 0))))
 
 
 ;;; テンプレート
 (setq org-capture-templates
-      '(("l" "Lifelog" plain
-         (file+datetree "~/Dropbox/documents/org/memo/lifelog.org" nil)
+      `(("t" "todo" entry
+         (file+datetree "~/documents/notes/todo.org")
+         "* TODO %?"
+         :unnarrowed t)
+        ("l" "lifelog" plain
+         (file+datetree "~/documents/notes/lifelog.org" ,(today-string))
          nil
          :unnarrowed t)
-        ("a" "Arch Linux Memo" plain
-         (file "~/Dropbox/documents/org/memo/arch.org")
-         nil
-         :immediate-finish t :jump-to-captured t)
-        ("s" "Ubuntu Server Memo" plain
-         (file "~/Dropbox/documents/org/memo/ubuntuserver.org")
-         nil
-         :immediate-finish t :jump-to-captured t)
-        ("w" "Want to do" plain
-         (file "~/Dropbox/wanttodo.org")
-         nil
-         :immediate-finish t :jump-to-captured t)
-        ("z" "Lab Memo" plain
-         (file (format "~/Dropbox/lab/memo/%s.org"
-                       (skk-default-current-date (skk-current-date-1) nil 0 t 0 0 0)))
-         nil
-         :immediate-finish t :jump-to-captured t)
-        ("b" "Blog" plain
-         (file (format-time-string "~/Dropbox/documents/org/blog/%Y-%m-%d.org" (current-time)))
-         (file "~/Dropbox/documents/org/blog/template.org")
-         :immediate-finish t :jump-to-captured t)
-        ("n" "Blog Memo" plain
-         (file "~/Dropbox/documents/org/blog/neta.org")
-         nil
-         :immediate-finish t :jump-to-captured t)
-        ("q" "Qiita" plain
-         (file (format-time-string "~/Dropbox/documents/org/qiita/%Y-%m-%d.org" (current-time)))
-         nil
-         :immediate-finish t :jump-to-captured t)))
+        ("p" "PHP Manual" plain
+         (file+datetree "~/documents/notes/lifelog.org" ,(today-string))
+         "**** PHP マニュアル ( まで)\n     - %?"
+         :unnarrowed t)
+        ("d" "daily report" plain
+         (file (format-time-string "~/documents/daily_reports/%Y-%m-%d.org" (current-time)))
+         (file "~/documents/daily_reports/template.org"))))
 
 (global-set-key (kbd "C-z") 'org-capture)
