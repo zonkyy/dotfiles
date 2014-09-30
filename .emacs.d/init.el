72fe2d0091883cf242c9c9347e5ededad1d8e2ec
akisute3 <akisute3@gmail.com>
2014-09-30 10:23:20 +0900

    init-loader-x.el を elisp ディレクトリに追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,3 @@
 ;;; (install-elisp "http://www.rubyist.net/~rubikitch/archive/init-loader-x.el")
-(require 'init-loader-x "~/.emacs.d/elisp/auto-install/init-loader-x")
+(require 'init-loader-x "~/.emacs.d/elisp/init-loader-x")
 (init-loader-load "~/.emacs.d/inits")
