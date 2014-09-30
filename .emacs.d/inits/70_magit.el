389891365e4cd978258e94f907bfecc6253dca39
akisute3 <akisute3@gmail.com>
2014-09-30 18:20:52 +0900

    magit で git-svn のコマンドを使用する設定を追加

--- .emacs.d/inits/70_magit.el
+++ .emacs.d/inits/70_magit.el
@@ -6,3 +6,9 @@
 (global-set-key (kbd "C-x v L") 'magit-key-mode-popup-logging)
 (global-set-key (kbd "C-x v z") 'magit-stash)
 (define-key magit-mode-map "\M-l" "l-all")
+
+
+;;; magit で git-svn のコマンドを使用する
+(use-package magit-svn
+  :ensure magit-svn)
+(add-hook 'magit-mode-hook 'magit-svn-mode)
