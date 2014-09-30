f95d67410907c7464eb20d404ee873633775f5a8
akisute3 <akisute3@gmail.com>
2014-09-30 11:13:49 +0900

    exec-path-from-shell の設定を migemo の設定ファイルから分離

---
+++ .emacs.d/inits/01_exec-path-from-shell.el
@@ -0,0 +1,4 @@
+;;;; シェルの環境変数を Emacs で使用する
+
+(use-package exec-path-from-shell
+  :ensure exec-path-from-shell)
--- .emacs.d/inits/07_migemo.el
+++ .emacs.d/inits/07_migemo.el
@@ -1,15 +1,13 @@
 ;;;; ローマ字で日本語をインクリメンタルサーチ
 
-
-;;; 基本設定
 (use-package migemo
   :ensure migemo)
+
+;;; 基本設定
 (setq migemo-command "cmigemo")
 (setq migemo-options '("-q" "--emacs"))
 
 ;;; migemo-dict のパスを指定 (環境変数 MIGEMO-DICT-PATH を取得)
-(use-package exec-path-from-shell
-  :ensure exec-path-from-shell)
 (setq migemo-dictionary (exec-path-from-shell-copy-env "MIGEMO_DICT_PATH"))
 
 (setq migemo-user-dictionary nil)
