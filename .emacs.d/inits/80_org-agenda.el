c66253a0094241c698f83889800b92426fbe4741
akisute3 <akisute3@gmail.com>
2014-10-30 15:09:47 +0900

    org-mode で子を全て DONE にしたとき親を自動で DONE にする機能削除

--- .emacs.d/inits/80_org-agenda.el
+++ .emacs.d/inits/80_org-agenda.el
@@ -4,9 +4,3 @@
 (setq org-todo-keywords
        '((sequence "TODO" "|" "DONE" "DELEGATED")))
 (global-set-key (kbd "C-*") 'org-insert-todo-heading)
-
-(defun org-summary-todo (n-done n-not-done)
-  "すべてのサブツリーが終了するとDONEに切り替え、その他の場合は、TODOにする。"
-  (let (org-log-done org-log-states)
-    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
