ec4faef0b4259d3044dba9e4c3778e9a4681da07
akisute3 <akisute3@gmail.com>
2015-01-22 09:49:09 +0900

    web-mode のインデントを 2 に設定

--- .emacs.d/inits/76_web.el
+++ .emacs.d/inits/76_web.el
@@ -3,6 +3,8 @@
 (use-package web-mode
   :ensure web-mode)
 
+(setq web-mode-markup-indent-offset 2)
+
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
