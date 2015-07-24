6f3d040673cc100556a00a06d51bceb4d458ca1b
akisute3 <akisute3@gmail.com>
2015-07-24 15:00:06 +0900

    foreing-regexp から pcre2el に乗り換え

--- .emacs.d/inits/06_foreign-regexp.el
+++
@@ -1,9 +0,0 @@
-;;;; Ruby の正規表現で検索・置換
-
-(use-package foreign-regexp
-  :ensure foreign-regexp)
-(custom-set-variables
- '(foreign-regexp/regexp-type 'ruby)
- '(reb-re-syntax 'foreign-regexp))
-
-(global-set-key (kbd "C-l r") 'foreign-regexp/query-replace)
