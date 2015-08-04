070784aac8c97dd6f58bf6d3bb68e16e50d980d9
akisute3 <akisute3@gmail.com>
2015-08-04 17:55:11 +0900

    PHP 関係の設定調整
    - php-eldoc のエラー対処
    - php-auto-yasnippets 導入
    - C-j による改行を newline-and-indent に変更

--- .emacs.d/inits/71_php.el
+++ .emacs.d/inits/71_php.el
@@ -3,18 +3,23 @@
 (use-package php-mode
   :ensure php-mode)
 
+(use-package php-auto-yasnippets
+  :ensure t)
+
 
 (defun php-mode-hooks ()
   (php-enable-psr2-coding-style)
   (ggtags-mode)
-  (php-eldoc-enable)
 
   ;; flycheck
   (flycheck-mode t)
 
   ;; keybind
+  (local-set-key (kbd "C-j") 'newline-and-indent)
   (local-set-key (kbd "C-c c") 'smart-compile)
   (local-set-key (kbd "C-c C-c") (kbd "C-c c C-m"))
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (local-set-key (kbd "<tab>") 'yas/create-php-snippet)
 
   ;; php-mode で削除されるので再定義
   (add-hook 'before-save-hook 'delete-trailing-whitespace))
--- .emacs.d/inits/72_php-eldoc.el
+++ .emacs.d/inits/72_php-eldoc.el
@@ -2,3 +2,12 @@
 
 (use-package php-eldoc
   :ensure php-eldoc)
+
+
+;;; auto-complete に関係するコードを除去
+(defun php-eldoc-enable ()
+  (interactive)
+  (setq-local eldoc-documentation-function 'php-eldoc-function)
+  (eldoc-mode 1))
+
+(add-hook 'php-mode-hook 'php-eldoc-enable)
