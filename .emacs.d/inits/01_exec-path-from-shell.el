add6439f3d13fa782b5c78d95f991c2a726dc82f
akisute3 <akisute3@gmail.com>
2015-08-04 17:52:57 +0900

    exec-path-from-shell を Tmux 自動実行に対応

--- .emacs.d/inits/01_exec-path-from-shell.el
+++ .emacs.d/inits/01_exec-path-from-shell.el
@@ -3,4 +3,8 @@
 (use-package exec-path-from-shell
   :ensure exec-path-from-shell)
 
+;;; oh-my-zsh で ZSH_TMUX_AUTOSTART = true のとき
+;;; 環境変数を取得できない問題への対策
+(setenv "ZSH_TMUX_AUTOQUIT" "false")
+
 (exec-path-from-shell-initialize)
