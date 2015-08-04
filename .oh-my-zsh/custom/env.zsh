ff29011a5c4319a47765becfe20942cc89ed98ec
akisute3 <akisute3@gmail.com>
2015-08-04 15:08:09 +0900

    環境変数の設定追加

---
+++ .oh-my-zsh/custom/env.zsh
@@ -0,0 +1,8 @@
+export ZSH_TMUX_AUTOSTART=true
+export RAILS_ENV=development
+export EDITOR=vim
+
+export MIGEMO_DICT_PATH="/usr/local/share/migemo/utf-8/migemo-dict"
+export DDSKK_LOAD_PATH="/home/ope/.emacs.d/elisp/ddskk-15.1"
+export GTAGSCONF=/usr/local/share/gtags/gtags.conf
+export GTAGSLABEL=pygments
