c2b2845f75afdf5fec98098af7f51c0fed755796
akisute3 <akisute3@gmail.com>
2015-08-07 15:38:45 +0900

    multiple-cursors に cua-mode のような連番挿入機能を追加

--- .emacs.d/inits/12_multiple-cursors.el
+++ .emacs.d/inits/12_multiple-cursors.el
@@ -14,4 +14,29 @@
     ("C-a"      . 'mc/edit-beginnings-of-lines)
     ("C-e"      . 'mc/edit-ends-of-lines)
     ("C-s"      . 'mc/mark-all-in-region)
-    ("M-n"      . 'mc/insert-numbers)))
+    ("M-n"      . 'my/mc/insert-numbers)))
+
+
+;; CUA モードのような連番挿入機能を追加
+(defvar my/mc/insert-numbers-hist nil)
+(defvar my/mc/insert-numbers-inc 1)
+(defvar my/mc/insert-numbers-pad "%01d")
+
+(defun my/mc/insert-numbers (start inc pad)
+  "Insert increasing numbers for each cursor specifically."
+  (interactive
+   (list (read-number "Start from: " 0)
+         (read-number "Increment by: " 1)
+         (read-string "Padding (%01d): " nil my/mc/insert-numbers-hist "%01d")))
+  (setq mc--insert-numbers-number start)
+  (setq my/mc/insert-numbers-inc inc)
+  (setq my/mc/insert-numbers-pad pad)
+  (mc/for-each-cursor-ordered
+   (mc/execute-command-for-fake-cursor
+    'my/mc--insert-number-and-increase
+    cursor)))
+
+(defun my/mc--insert-number-and-increase ()
+  (interactive)
+  (insert (format my/mc/insert-numbers-pad mc--insert-numbers-number))
+  (setq mc--insert-numbers-number (+ mc--insert-numbers-number my/mc/insert-numbers-inc)))
