8af9916cd0930b46e6cb5bb001ab69a2535ecb26
akisute3 <akisute3@gmail.com>
2015-08-07 14:09:38 +0900

    選択範囲で isearch する機能を追加

--- .emacs.d/inits/01_misc.el
+++ .emacs.d/inits/01_misc.el
@@ -178,3 +178,16 @@
       (delete-file
         (buffer-file-name (current-buffer)))
       (kill-buffer (current-buffer)))))
+
+;;; 選択範囲を isearch
+(defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+  (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+      (progn
+        (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+        (deactivate-mark)
+        ad-do-it
+        (if (not forward)
+            (isearch-repeat-backward)
+          (goto-char (mark))
+          (isearch-repeat-forward)))
+    ad-do-it))
