e2ad8a80cb7104862b15a1340c36baaaea885682
akisute3 <akisute3@gmail.com>
2016-05-13 10:44:16 +0900

    ace-isearch 廃止

--- .emacs.d/inits/09_ace-isearch.el
+++
@@ -1,8 +0,0 @@
-;;;; isearch 起動時、文字数に合わせて ace-jump-mode や helm-swoop に切り替える
-
-(use-package ace-isearch
-  :ensure ace-isearch)
-
-(global-ace-isearch-mode 1)
-(setq ace-isearch-use-ace-jump nil)
-;; (setq ace-isearch-input-idle-jump-delay 0.3)
