2612a3d82fe3f916e15b7bcde5b8a60871059125
akisute3 <akisute3@gmail.com>
2016-05-13 11:06:24 +0900

    org-capture のテンプレート追加

--- .emacs.d/inits/80_org-capture.el
+++ .emacs.d/inits/80_org-capture.el
@@ -2,26 +2,30 @@
 
 (use-package org-capture)
 
-(use-package skk-vars)
-(defun today-string ()
-  (skk-current-date
-   (lambda (date-information format gengo and-time)
-     (skk-default-current-date date-information nil 0 'gengo 0 0 0))))
+;;; skk-funcs.el でオーバーライドされる
+(defun today-string ())
 
 
 ;;; テンプレート
 (setq org-capture-templates
       `(("t" "todo" entry
          (file+datetree "~/documents/notes/todo.org")
-         "* TODO %?"
-         :unnarrowed t)
+         "* TODO %? %^g")
         ("l" "lifelog" plain
          (file+datetree "~/documents/notes/lifelog.org" ,(today-string))
+         nil
+         :unnarrowed t)
+        ("c" "config" plain
+         (file+datetree "~/documents/notes/config.org" ,(today-string))
          nil
          :unnarrowed t)
         ("p" "PHP Manual" plain
          (file+datetree "~/documents/notes/php.org" ,(today-string))
          "**** PHP マニュアル ( まで)\n     - %?"
+         :unnarrowed t)
+        ("s" "Self Development" entry
+         (file+datetree "~/documents/notes/self_development.org")
+         "* %?"
          :unnarrowed t)
         ("d" "daily report" plain
          (file (format-time-string "~/documents/daily_reports/%Y-%m-%d.org" (current-time)))
