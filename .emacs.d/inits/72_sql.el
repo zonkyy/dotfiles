29d51f9e1b22ad8c6a78dc45cbd320b7b7b4ee66
akisute3 <akisute3@gmail.com>
2016-05-13 10:47:53 +0900

    SQL 編集用設定を追加

--- .emacs.d/inits/72_sql.el
+++ .emacs.d/inits/72_sql.el
@@ -1,5 +1,20 @@
 ;;; SQL 用の設定
 
-(add-hook 'sql-mode-hook
-          (lambda ()
-            (sql-highlight-mysql-keywords)))
+(use-package edbi
+  :ensure t)
+
+;;; 全件 SELECT は時間がかかるので 1000 件に絞る
+(defun edbi:dbview-show-data-sql (conn table-name)
+  (if edbi:dbview-show-table-data-default-limit
+      (edbi:dbd-limit-format-fill
+       (edbi:dbd-get conn) table-name
+       edbi:dbview-show-table-data-default-limit)
+    (format "SELECT * FROM %s LIMIT 1000" table-name)))
+
+
+(defun sql-mode-hooks ()
+  (sql-highlight-mysql-keywords)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  (setq indent-line-function (quote insert-tab)))
+(add-hook 'sql-mode-hook 'sql-mode-hooks)
