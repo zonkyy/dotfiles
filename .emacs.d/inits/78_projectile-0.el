4ee2d1b95324b61bfec5f34f50572afcf2bfc7b8
akisute3 <akisute3@gmail.com>
2016-05-13 10:56:38 +0900

    projectile の設定調整

--- .emacs.d/inits/78_projectile-0.el
+++ .emacs.d/inits/78_projectile-0.el
@@ -1,8 +1,9 @@
 ;;;; プロジェクト管理をする projectile-mode
 
 (use-package projectile
-  :ensure projectile)
+  :ensure t)
 (projectile-global-mode)
 
-;;; ファイル選択に helm を使用する
-(setq projectile-completion-system 'helm)
+(use-package helm-projectile
+  :ensure t)
+(helm-projectile-on)
