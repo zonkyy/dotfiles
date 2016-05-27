5a6abc2fd5e5913b577b64f57eb7c41093bf8702
ope <ope@localhost.localdomain>
2016-05-27 17:51:04 +0900

    GDB のコマンドをキーで実行する設定の追加

--- .emacs.d/inits/71_c.el
+++ .emacs.d/inits/71_c.el
@@ -19,6 +19,44 @@
 (ad-activate 'gdb-set-window-buffer)
 
 
+;;; GDB 実行時にキーでデバッグを行う設定
+(defun gdb-quit-view-mode ()
+  (interactive)
+  (view-mode 0)
+  (delete-other-windows))
+
+(defvar gdb-view-keybinds
+  `((" " . gud-next)
+    ("i" . gud-step)
+    ("b" . gud-break)
+    ("u" . gud-remove)
+    ("g" . gud-cont)
+    ("h" . gud-until)
+    ("p" . gud-print-prompt)
+    ("r" . gud-run)
+    ("R" . gud-run-from-stdin)))
+
+(defun gdb-add-view-mode-to-source-file (source-file-name)
+  (with-current-buffer (get-file-buffer source-file-name)
+    (view-mode)
+    (define-many-keys view-mode-map gdb-view-keybinds)
+    (define-key view-mode-map (kbd "q") 'gdb-quit-view-mode)))
+
+(defun gdb-mode-hooks ()
+  (gud-def gud-print-prompt
+           (gud-call (format "print %s" (region-or-prompt "print")))
+           nil)
+  (gud-def gud-run-from-stdin
+           (gud-call (format "run < %s" (read-file-name "run < ")))
+           nil)
+  ;; XXX: gdb-main-file が更新されるのを無理矢理待つ
+  (sit-for 0.1)
+  (gdb-add-view-mode-to-source-file gdb-main-file)
+  (next-window-except-ignore-window))
+
+(add-hook 'gdb-mode-hook 'gdb-mode-hooks)
+
+
 ;;; hooks
 (defun c/c++-mode-hooks ()
   ;; google-c-style
