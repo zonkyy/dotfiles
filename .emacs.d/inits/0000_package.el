ac376d74846adcc2cddba0ec16ef096f2373c413
akisute3 <akisute3@gmail.com>
2016-06-06 21:49:44 +0900

    MELPA の URL を更新

--- .emacs.d/inits/0000_package.el
+++ .emacs.d/inits/0000_package.el
@@ -3,6 +3,6 @@
 (require 'package)
 
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
