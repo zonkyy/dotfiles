666ebbafcfc2e2312d577ede33607de501dc5129
yynozk <yynozk@gmail.com>
2017-08-29 18:07:19 +0900

    init.el 追加

---
+++ .emacs.d/init.el
@@ -0,0 +1,12 @@
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+
+(require 'use-package)
+
+(use-package init-loader
+  :ensure t
+  :init
+  (init-loader-load))
